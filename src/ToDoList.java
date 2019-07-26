d92ac995417bbfafcb760f9cd0d3fb12091cda87
Justin Ang <justin.angzw@ncs.com.sg>
2019-07-26 10:17:07 +0800

    Monday's workshop commit.

---
+++ src/ToDoList.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+public class ToDoList
+{
+	private HashMap<String, Task> tasks = new HashMap<String, Task>();
+
+	public void addTask(Task task)
+	{
+		tasks.put(task.getDescription(), task);
+	}
+
+	public void completeTask(String description)
+	{
+		getTask(description).setComplete(true);
+	}
+
+	public boolean getStatus(String description)
+	{
+		return getTask(description).isComplete();
+	}
+
+	public Task getTask(String description)
+	{
+		return tasks.get(description);
+	}
+
+	public Task removeTask(String description)
+	{
+		return tasks.remove(description);
+	}
+
+	public Collection<Task> getAllTasks()
+	{
+		return tasks.values();
+	}
+
+	public Collection<Task> getCompletedTasks()
+	{
+		HashMap<String, Task> hComplTasks = new HashMap<String, Task>(tasks);
+
+		for (Task t : hComplTasks.values())
+			if (!t.isComplete())
+				hComplTasks.remove(t.getDescription());
+
+		return hComplTasks.values();
+	}
+}
---
+++ test/ToDoListTest.java
@@ -0,0 +1,77 @@
+import java.util.Collection;
+
+import org.junit.*;
+import org.junit.Test;
+import junit.framework.*;
+
+public class ToDoListTest extends TestCase
+{
+	// Define Test Fixtures
+	ToDoList tdl;
+	Task t;
+
+	final String tDesc = "This do be a task mateys";
+
+	public ToDoListTest()
+	{
+		super();
+	}
+
+	@Before
+	public void setUp() throws Exception
+	{
+		// Initialise Test Fixtures
+		tdl = new ToDoList();
+		t = new Task(tDesc, false);
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+		// Uninitialise test Fixtures
+	}
+
+	@Test
+	public void testGetAllTasks()
+	{
+		// Check if new Todo List
+		assertTrue(tdl.getAllTasks().isEmpty());
+		tdl.addTask(t);
+		assertFalse(tdl.getAllTasks().isEmpty());
+	}
+
+	@Test
+	public void testAddTask()
+	{
+		assertTrue(tdl.getAllTasks().isEmpty());
+		tdl.addTask(t);
+		assertFalse(tdl.getAllTasks().isEmpty());
+	}
+
+	@Test
+	public void testgetStatus()
+	{
+		tdl.addTask(t);
+		assertEquals(tdl.getStatus(tDesc), false);
+	}
+
+	@Test
+	public void testRemoveTask()
+	{
+		tdl.addTask(t);
+		assertFalse(tdl.getAllTasks().isEmpty());
+		tdl.removeTask(tDesc);
+		assertTrue(tdl.getAllTasks().isEmpty());
+	}
+
+	@Test
+	public void testGetCompletedTasks()
+	{
+		tdl.addTask(t);
+		Collection<Task> cTask = tdl.getCompletedTasks();
+		assertTrue(cTask.isEmpty());
+		tdl.completeTask(tDesc);
+		cTask = tdl.getCompletedTasks();
+		assertFalse(cTask.isEmpty());
+	}
+}
