b83f74d333eafb19b1488f8b754f8da0f9d31bec
Justin Ang <justin.angzw@ncs.com.sg>
2019-07-26 14:36:01 +0800

    Delete task added to main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Scanner;
 
@@ -18,7 +17,7 @@ public class Main
 
 	public static void addTask(Scanner in)
 	{
-		System.out.print("Add Task: Enter a task description: ");
+		System.out.print("Add Task - Enter a task description: ");
 		String input = in.nextLine();
 		if (addTask(input))
 			System.out.println("Added new task successfully with desc: " + input);
@@ -29,13 +28,25 @@ public class Main
 	public static void viewTasks()
 	{
 		Collection<Task> tal = tdl.getAllTasks();
-		
+
 		System.out.println("View All Tasks - There are currently " + tal.size() + " tasks in the list:");
-		
-		for(Task t : tal)
+
+		for (Task t : tal)
 		{
 			System.out.println("Task: " + t.getDescription());
 		}
+	}
+
+	public static void deleteTask(Scanner in)
+	{
+		System.out.print("Delete Task - Enter a task description to delete: ");
+		
+		String taskDesc = in.nextLine();
+		
+		Task t = tdl.removeTask(taskDesc);
+		
+		if (t != null)
+			System.out.println("Successfully removed task: " + t.getDescription());
 	}
 
 	public static void main(String[] args)
@@ -62,6 +73,9 @@ public class Main
 				break;
 			case 2:
 				viewTasks();
+				break;
+			case 3:
+				deleteTask(in);
 				break;
 			case 99:
 				in.close();
